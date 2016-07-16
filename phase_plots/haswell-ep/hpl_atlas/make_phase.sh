2359d98f612cdb7b0cbd1d1c4685fa96187688c8
Vince Weaver <vincent.weaver@maine.edu>
2016-07-16 15:28:06 -0400

    add phase plot generation scripts

---
+++ phase_plots/haswell-ep/hpl_atlas/make_phase.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#/home/vince/research/linux-kernel/linux-4.1.5/tools/perf/perf stat -a -e cycles,instructions,cache-misses,cache-references,uncore_imc_0/cas_count_read/,uncore_imc_0/cas_count_write/,power/energy-cores/,power/energy-pkg/,power/energy-ram/ -I 100 -x , ./run_test.sh 2> sleep.perf.phase 1> sleep.stdio.phase ; done
+
+/home/vince/research/linux-kernel/linux-4.1.5/tools/perf/perf stat -a -e power/energy-ram/ -I 100 -x , mpirun.mpich -np 4 ./xhpl 2> hpl_atlas.perf.phase 1> hpl_atlas.stdio.phase ; done
---
+++ phase_plots/haswell-ep/stream/make_phase.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#/home/vince/research/linux-kernel/linux-4.1.5/tools/perf/perf stat -a -e cycles,instructions,cache-misses,cache-references,uncore_imc_0/cas_count_read/,uncore_imc_0/cas_count_write/,power/energy-cores/,power/energy-pkg/,power/energy-ram/ -I 100 -x , ./run_test.sh 2> sleep.perf.phase 1> sleep.stdio.phase ; done
+
+/home/vince/research/linux-kernel/linux-4.1.5/tools/perf/perf stat -a -e power/energy-ram/ -I 100 -x , /home/vince/research/power-measurement/power_benchmarks/stream-5.10/stream_omp 2> stream.perf.phase 1> stream.stdio.phase ; done
